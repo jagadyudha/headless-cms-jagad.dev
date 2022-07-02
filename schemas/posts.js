6de2647ceae8bb0d76a486ca17d2aebbc81fe885
unknown <jagadyudhaawali@gmail.com>
2022-07-02 23:47:51 +0700

    feat: slug schema

--- schemas/posts.js
+++ schemas/posts.js
@@ -4,9 +4,19 @@ export default {
   title: "posts",
   fields: [
     {
-      name: "slug",
+      name: "title",
       type: "string",
+      title: "title",
+    },
+    {
       title: "slug",
+      name: "slug",
+      type: "slug",
+      options: {
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+      },
     },
     {
       name: "markdown",
